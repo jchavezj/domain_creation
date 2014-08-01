00fd127b4a711898f0cd00aa88e5d4e5092e4fff
johnny chavez <johnny_chavez@rocketmail.com>
2014-08-01 11:34:14 -0500

    removed the GLP within the script

--- vhcreator.sh
+++ vhcreator.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
 #
 ############################################################################
-#     This file is part of apache_vhost script.				   #
-# 									   #
-#    Apache_vhsot is free software: you can redistribute it and/or modify  #
-#    it under the terms of the GNU General Public License as published by  #
-#    the Free Software Foundation, either version 3 of the License, or     #
-#    (at your option) any later version.				   #
-# 									   #
-#    apache_vhost is distributed in the hope that it will be useful,       #
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of        #
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
-#    GNU General Public License for more details.			   #
-# 									   # 
-#    You should have received a copy of the GNU General Public License     #
-#    along with apache_vhost.  If not, see <http://www.gnu.org/licenses/>. #
-#									   #
-#  									   #
+#     This file is part of vhcreator.sh script.				   #
+  									   #
 # <author> Johnny Chavez </author>            				   #
 # <date>2014-07-29</date>						   #
 # V. 0.1								   #
